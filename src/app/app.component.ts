374694b34d327f1fee5b1c8a4687f7fb281ea0d9
Adam-Wallenrod <psredniawa@gmail.com>
2019-12-10 22:02:52 +0100

    Added toggleFlexDirection() for changing flex-direction;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'starr';
+  isRowDirection: boolean = true;
+
+
+  toggleFlexDirection(){
+    this.isRowDirection = !this.isRowDirection;
+  }
 }
